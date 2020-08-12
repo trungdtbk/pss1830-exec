1d62a88be35abf4d6f0e1a913ec12b265b853ff0
Trung Truong <trung.truong@nokia.com>
2020-08-12 13:02:50 +1200

    update PROMPT_RE to work with 32EC2

--- pssexec/pssroot.py
+++ pssexec/pssroot.py
@@ -5,7 +5,7 @@ from common import PSSException
 class PSSRoot(PSSCommon):
     """Wrapper for PSS root mode."""
 
-    PROMPT_RE = 'root@EC1830-\d+-\d+-ACT:/root[\r\n]*# $'
+    PROMPT_RE = '(root@EC1830-\d+-\d+-ACT:[/~\w\r\n]*# $)|(root@32EC2-\d+-\d+-ACT:[/~\w\r\n]*# $)'
 
     def open(self):
         super(PSSRoot, self).open()
